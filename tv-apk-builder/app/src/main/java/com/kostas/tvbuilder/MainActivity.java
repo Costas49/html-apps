48be8dc485ea232c89e37e7de92edc54a64371b5
Costas49 <140644068+Costas49@users.noreply.github.com>
2026-09-16 16:32:02 +0300

    Add safe generic Android TV WebView activity

---
+++ tv-apk-builder/app/src/main/java/com/kostas/tvbuilder/MainActivity.java
@@ -0,0 +1,51 @@
+package com.kostas.tvbuilder;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.webkit.WebViewAssetLoader;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle state) {
+        super.onCreate(state);
+        webView = new WebView(this);
+        setContentView(webView);
+        webView.setFocusable(true);
+        webView.setFocusableInTouchMode(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(false);
+        webView.getSettings().setAllowContentAccess(false);
+        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+
+        final WebViewAssetLoader loader = new WebViewAssetLoader.Builder()
+                .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(this))
+                .build();
+
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                return loader.shouldInterceptRequest(request.getUrl());
+            }
+        });
+
+        webView.loadUrl("https://appassets.androidplatform.net/assets/index.html");
+        webView.requestFocus();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}

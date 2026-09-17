a6eb0edad09b88085da3fcf93c009087e8bfaf70
Costas49 <140644068+Costas49@users.noreply.github.com>
2026-09-17 15:43:41 +0300

    Fix AGP 9 built-in Kotlin configuration

--- TVFileManager/app/build.gradle.kts
+++ TVFileManager/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -29,10 +28,6 @@ android {
             )
         }
     }
-}
-
-kotlin {
-    jvmToolchain(17)
 }
 
 dependencies {

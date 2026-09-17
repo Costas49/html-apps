e8fa95734ddf56533e4a1b80ddc4e350901e46a1
Costas49 <140644068+Costas49@users.noreply.github.com>
2026-09-17 16:44:06 +0300

    Add TV Compatibility Hub app build

---
+++ TVCompatHub/app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "gr.costas.tvcompathub"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "gr.costas.tvcompathub"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    // Framework APIs only: lightweight for Android TV boxes.
+}
